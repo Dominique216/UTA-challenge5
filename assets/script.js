92e3fbd9a64246779ad579173a83ddcd49514584
Dominique Nix <dominiquenix@Dominiques-MacBook-Air.local>
2023-01-09 20:24:57 -0600

    The changes now save in local storage and edits stay when page is reloaded

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,55 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
 
+$(function () {
+
+var saveBtn = $(".saveBtn")
+var description = $(".description")
+
+// this function handels saving the input from each time slot to the local storage
+function setBtn(num, obj, timeOfDay, string) {
+  saveBtn[num].addEventListener('click', function(){
+ obj = {
+  timeofDay: timeOfDay, 
+  todo: description[num].value.trim()
+ }
+ localStorage.setItem(string, JSON.stringify(obj))
+  })
+}
+
+setBtn(0, eight, 8, 'eight')
+setBtn(1, nine, 9, 'nine')
+setBtn(2, ten, 10, 'ten')
+setBtn(3, eleven, 11, 'eleven')
+setBtn(4, twelve, 12, 'twelve')
+setBtn(5, one, 1, 'one')
+setBtn(6, two, 2, 'two')
+setBtn(7, three, 3, 'three')
+setBtn(8, four, 4, 'four')
+setBtn(9, five, 5, 'five')
+
+
+// this function handles grabbing the changes from the local storage when the page is loaded
+function getLastChange(string, obj, num) {
+  var lastEdit = JSON.parse(localStorage.getItem(string))
+  console.log(lastEdit)
+  if(lastEdit !== null) {
+    description[num].textContent = lastEdit.todo
+  }
+}
+
+function init() {
+  getLastChange('eight', eight, 0)
+  getLastChange('nine', nine, 1)
+  getLastChange('ten', ten, 2)
+  getLastChange('eleven', eleven, 3)
+  getLastChange('twelve', twelve, 4)
+  getLastChange('one', one, 5)
+  getLastChange('two', two, 6)
+  getLastChange('three', three, 7)
+  getLastChange('four', four, 8)
+  getLastChange('five', five, 9)
+}
+
+init()
 
 
 // This section handles adding the current date to the header and will add and remove certain classes depending on the time of the day
